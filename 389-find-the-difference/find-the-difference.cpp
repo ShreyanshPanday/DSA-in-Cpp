393ee1591c1d388225322caaf0afefda33914252
ShreyanshPanday <shreyansh12620@gmail.com>
2026-09-21 11:15:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (77.47%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        sort(t.begin(), t.end());
-        sort(s.begin(), s.end());
+        int XOR = (int)t.back();
         for(int i = 0; i < s.size(); i++){
-            if(s[i] != t[i]) return t[i];
+            XOR ^= int(s[i] ^ t[i]);
         }
-        return t.back();
+        return (char)XOR;
     }
 };
